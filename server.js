0a1097a52443c31ad39ebdee9a871d6a4cf32e79
Kenneth Rebello <54711676+kenneth-rebello@users.noreply.github.com>
2020-01-09 16:43:41 +0530

    Fix 2
    Attempt 3 to deploy

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const compression = require('compression');
+const enforce = require('express-sslify');
 const connectDB = require('./config/db');
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(express.json({extended: false}));
 app.use(methodOverride('_method'));
 
 if(process.env.NODE_ENV === "production"){
+    app.use(bodyParser.urlencoded({extended: true}));
     app.use(compression());
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', (req, res)=>{
         res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'));
@@ -28,3 +30,7 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT ,()=> console.log(`Server started on ${PORT}`) ); 
+
+app.get('/service-worker.js', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, '..','build','service-worker.js'))
+})
